6614da9a7725d0c9813df3900117b74bf44e86f5
Andre <helberg.andre@gmail.com>
2016-02-25 00:09:30 +0200

    add delete callbacks

--- lib/active_record/resource/callbacks.rb
+++ lib/active_record/resource/callbacks.rb
@@ -8,24 +8,43 @@ module ActiveRecord
           # When a class inherits from AR::Base, all models do
           def self.inherited(child_class)
             # On the class that Inherits AR::Base
-            child_class.class_eval do
-               def self.new(*args)
-                 check_resource_methods
+            unless child_class == ActiveRecord::SchemaMigration
+              child_class.class_eval do
+                def self.new(*args)
+                  check_resource_methods
 
-                 super(*args)
-               end
+                  super(*args)
+                end
+
+                def handle_delete
+                  resource_delete_validations
+                  if self.errors.present?
+                    return false
+                  end
+                  return resource_delete
+                end
+              end
+
+              child_class.instance_eval do
+                # Add Hooks
+                before_create :resource_create
+                before_validation(:resource_create_validations, on: :create)
+
+                before_destroy :handle_delete
+              end
             end
 
             super
           end
 
-          # Add Hooks
-          before_create :resource_create
-          before_validation(:resource_create_validations, on: :create)
+
 
           def self.check_resource_methods
             raise not_creatable_error unless method_defined? :resource_create
             raise no_creation_validations_error unless method_defined? :resource_create_validations
+
+            raise not_deletable_error unless method_defined? :resource_delete
+            raise no_deletable_validations_error unless method_defined? :resource_delete_validations
           end
 
           def self.not_creatable_error
@@ -34,6 +53,14 @@ module ActiveRecord
 
           def self.no_creation_validations_error
             "The model #{self} is createable, but does not implement #resource_create_validations"
+          end
+
+          def self.not_deletable_error
+            "The model #{self} is deletable, but does not implement #resource_delete"
+          end
+
+          def self.no_deletable_validations_error
+            "The model #{self} is deletable, but does not implement #resource_delete_validations"
           end
         end
       end
